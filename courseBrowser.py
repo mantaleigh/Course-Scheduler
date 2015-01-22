500fb6cfe0c53d9b12b811b57302d6938d2d93b1
mantaleigh <samanthalvoigt@gmail.com>
2015-01-21 21:26:09 -0600

    include scrollbars, create criteria boxes

--- courseBrowser.py
+++ courseBrowser.py
@@ -4,6 +4,52 @@ from Tkinter import *
 import HTMLscraper as scraper
 import multiListBox as mlb
 
+# http://tkinter.unpythonic.net/wiki/VerticalScrolledFrame
+class VerticalScrolledFrame(Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+
+    """
+    def __init__(self, parent, *args, **kw):
+        Frame.__init__(self, parent, *args, **kw)            
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
+
+
 class CourseBrowserApp(Tk): 
     def __init__(self): 
         Tk.__init__(self)
@@ -13,8 +59,8 @@ class CourseBrowserApp(Tk):
         #this data needs to be stored somewhere else, and scraped w/ the other data
         self.distributions = ['Arts, Music, Theatre, Film, Video', 'Epistemology & Cognition', \
         'Historical Studies', 'Language & Literature', 'Mathematical Modeling', \
-        'Natural & Physical Sciences', 'QRB', 'QRF', 'Religion, Ethics, & Moral Philosophy', \
-        'Social & Behavioral Analysis']
+        'Natural & Physical Sciences', 'Religion, Ethics, & Moral Philosophy', \
+        'Social & Behavioral Analysis', 'QRB', 'QRF']
         self.subjects = ['Africana Studies', 'American Studies', 'Anthropology', 'Arabic', \
         'Art History', 'Art-Studio', 'Astronomy', 'Biochemistry', 'Biological Science', \
         'Cinema and Media Studies', 'Chemistry', 'Chinese Language and Culture', \
@@ -44,24 +90,29 @@ class CourseBrowserApp(Tk):
         'Religion':'REL', 'Russian':'RUSS', 'South Asia Studies':['HNUR','SAS'], 'Sociology':'SOC', \
         'Spanish':'SPAN', 'Sustainability':'SUST', 'Theatre Studies':'THST', 'Women\'s and Gender Studies':'WGST',\
         'Writing':'WRIT'}
+
+        self.createdOptions = []
+        self.optionButtonClicked = False
         
         self.createFrames()
         self.createWidgets()
         
     def createFrames(self): 
+        '''Creates the frames (and a canvas) to organize all the information in the main app and create scrollbars'''
+        self.mainFrame = VerticalScrolledFrame(self)
+        self.mainFrame.pack()
         
-        # Frames
-        self.topFrame = Frame(self, bd=2, relief=GROOVE, takefocus=True)
+        self.topFrame = Frame(self.mainFrame.interior, bd=2, relief=GROOVE, takefocus=True)
         self.topFrame.pack()
         self.topFrameLeft = Frame(self.topFrame, takefocus=True, bd=2, relief=GROOVE)
         self.topFrameLeft.pack(side=LEFT)
         self.topFrameRight = Frame(self.topFrame, takefocus=True, bd=2, relief=GROOVE)
         self.topFrameRight.pack(side=RIGHT)
-        #self.middleFrame = Frame(self, bd=2, relief=GROOVE, takefocus=True)
-        #self.middleFrame.pack()
-        self.bottomFrame = Frame(self, bd=2, relief=GROOVE, takefocus=True)
+        self.middleFrame = Frame(self.mainFrame.interior, bd=2, relief=GROOVE, takefocus=True)
+        self.middleFrame.pack()
+        self.bottomFrame = Frame(self.mainFrame.interior, bd=2, relief=GROOVE, takefocus=True)
         self.bottomFrame.pack()
-        self.buttonFrame = Frame(self, bd=2, relief=GROOVE, takefocus=True)
+        self.buttonFrame = Frame(self.mainFrame.interior, bd=2, relief=GROOVE, takefocus=True)
         self.buttonFrame.pack()
    
     def createWidgets(self): 
@@ -70,29 +121,31 @@ class CourseBrowserApp(Tk):
         searchByLabel = Label(self.topFrameLeft, text='SEARCH BY:', fg='navy',font='Times 16 bold')
         searchByLabel.grid(row=0, column=0, sticky=W+E, padx=20)
         searchByBoxes = ['Distribution', 'Subject', 'Department', 'Time/Days', 'Professor']
-        searchByVars = []
         colAvail = 1
+        self.searchVars = {}
         for box in searchByBoxes: 
             var = IntVar()
             button = Checkbutton(self.topFrameLeft,text=box,variable=var)
             button.grid(row=0, column=colAvail, sticky=W+E)
-            searchByVars.append(var)
+            self.searchVars[box] = var
             colAvail += 1
       
         # 'Show Only' section ------->
         showOnlyLabel = Label(self.topFrameRight, text='SHOW ONLY:', fg='navy', font='Times 16 bold')
         showOnlyLabel.grid(row=0, column=0, sticky=W+E, padx=20)
         colAvail = 1
+        self.showVars = {}
         showOnlyBoxes = ['If seats are available*','100 levels','200 levels','300 levels']
-        showOnlyVars = []
         for box in showOnlyBoxes: 
             var = IntVar()
             button = Checkbutton(self.topFrameRight, text=box, variable=var)
             button.grid(row=0, column=colAvail, sticky=W+E)
-            showOnlyVars.append(var)
+            self.showVars[box] = var
             colAvail += 1
+            
+        criteriaOptionsButton = Button(self.middleFrame, text="Give me options!", command=self.createCriteriaOptions)
+        criteriaOptionsButton.pack(fill=X)
       
-        
         # Results ("Courses That Fit") section -------->
         coursesThatFitLabel = Label(self.bottomFrame, text="COURSES THAT FIT YOUR CRITERIA:", \
         font='Times 18 bold', fg='navy')
@@ -110,9 +163,40 @@ class CourseBrowserApp(Tk):
         updateButton.grid(row=0, column=0)
         makeScheduleButton = Button(self.buttonFrame, text="Add Course To Schedule")
         makeScheduleButton.grid(row=0, column=1)
-        notes = Label(self.buttonFrame, text="Note that some fields may not reflect recent changes, and that updating the courses may take a few minutes.", 
-        font='Times 12 italic')
+        notes = Label(self.buttonFrame, text="Note that some fields may not reflect recent changes, \
+        and that updating the courses may take a few minutes.", font='Times 12 italic')
         notes.grid(row=1, columnspan=2)
+
+    def checkSearchButtons(self): 
+        '''Check the state of the 'Search By' checkbuttons and returns a list w/the name of those that were clicked '''
+        criteriaList = []
+        for key, value in self.searchVars.items(): 
+            state = value.get()
+            if state != 0: 
+                criteriaList.append(key)
+        return criteriaList
+        
+    def checkShowButtons(self): 
+        '''Check the state of the 'Show Only' checkbuttons and returns a list w/the name of those that were clicked. '''
+        criteriaList = []
+        for key, value in self.showVars.items(): 
+            state = value.get()
+            if state != 0: 
+                criteriaList.append(key)
+        return criteriaList
+    
+    def createCriteriaOptions(self):
+        if self.optionButtonClicked == False: #only create the showCoursesButton when options are displaying
+            showCoursesButton = Button(self.middleFrame, text="Show me courses!", command=None)
+            showCoursesButton.pack(fill=X)
+            self.optionButtonClicked = True
+        criteriaList = self.checkSearchButtons()
+        print criteriaList #testing
+        for option in criteriaList:
+            if option not in self.createdOptions:
+                optionFrame = ScrollableOptionFrame(self.middleFrame, option, self.distributions, self.subjects, self.departments)
+                optionFrame.pack(side=LEFT)
+                self.createdOptions.append(option)
 
 class CourseResultsBox(): 
         def __init__(self, master, **kwargs): 
@@ -128,9 +212,10 @@ class CourseResultsBox():
         def grid_(self, **kwargs): 
             self.box.grid(**kwargs)
         
-        def onReturn(self, event): 
+        def onReturn(self, event):
+            '''Creates the extra info toplevel frame when return is hit while selecting a course in the listbox''' 
             if self.extraInfoApp!=None: self.extraInfoApp.destroy()
-            CRN = self.box.get(self.box.curselection())[0]
+            CRN = self.box.get(self.box.curselection())[0] #get CRN (the first value) from the selection
             allCourses = scraper.readCourseFile()
             selectedCourseInfo = (item for item in allCourses if item['CRN'] == CRN).next()
             description = selectedCourseInfo['Description']
@@ -164,11 +249,12 @@ class CourseResultsBox():
                             for key in keys: 
                                 combinedVals = combinedVals + ' | ' + key + ': ' + dictionary[key]
                             courseInfo.append(combinedVals + ' |')
-                    except KeyError: 
-                        courseInfo.append('')
+                    except KeyError: #When the key doesn't exist (ex: for courses w/no additional instructor)
+                        courseInfo.append('') #use blank string
             
                 self.box.insert (END, courseInfo)  
         
+        # will be used to update the info in the listbox given the criteria
         def updateCourseInfo(self, columnHeaders): 
             pass
             
@@ -176,11 +262,11 @@ class ExtraInfoApp(Toplevel):
     def __init__(self, description, prereqs, additionalInstructor=None): 
         Toplevel.__init__(self)
         self.title('Extra Information')
-        #self.grid()
         self.createFrames(additionalInstructor)
         self.createWidgets(description, prereqs, additionalInstructor)
     
     def createFrames(self, additionalInstructor):
+        '''Creates the frames to hold all the extra information.'''
         self.descFrame = Frame(self, bd=2, relief=GROOVE, takefocus=True)
         self.descFrame.pack()
         self.prereqFrame = Frame(self, bd=2, relief=GROOVE, takefocus=True)
@@ -191,6 +277,7 @@ class ExtraInfoApp(Toplevel):
     
     #can probably pretty this up (less repetition) with a dictionary or list
     def createWidgets(self, description, prereqs, additionalInstructor):
+        '''Creates the widgets for the extra information (description, prerequisites, or additional instructor)'''
         descriptionLabel = Label(self.descFrame,font='Times 18 bold', fg='navy', text="DESCRIPTION:")
         descriptionLabel.pack()
         descriptionText = Message(self.descFrame, text=description)
@@ -204,6 +291,52 @@ class ExtraInfoApp(Toplevel):
             instructorLabel.pack()
             instructorText = Label(self.addtlInstFrame, text=additionalInstructor)
             instructorText.pack()
+
+class ScrollableOptionFrame(Frame):
+    def __init__(self, master, option, allDistributions, allSubjects, allDepartments, **options):
+        Frame.__init__(self, master, **options)
+        self.allDistributions = allDistributions
+        self.allSubjects = allSubjects
+        self.allDepartments = allDepartments
+        self.option = option
+        self.toSearchVars = {}
+        
+        self.canvas = Canvas(self, borderwidth=0)
+        self.frame = Frame(self.canvas, relief=SUNKEN)
+        self.vsb = Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+        self.canvas.config(scrollregion=(0,0,250,250))
+
+        self.vsb.pack(side=RIGHT, fill=Y)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        self.canvas.create_window((0,0), window=self.frame, anchor=N+W)
+
+        self.populate()
+
+    def populate(self):
+        '''populate the scrollable frame with all info'''
+        if self.option.upper() == "time/days".upper():
+            textStr = self.option.upper() + ":"
+        else:
+            textStr = self.option.upper() + "S:"
+        distLabel = Label(self.frame, text=textStr, fg='navy', font='Times 16 bold')
+        distLabel.pack(fill=X)
+        if self.option.upper() == "distribution".upper(): infoList = self.allDistributions
+        elif self.option.upper() == "subject".upper(): infoList = self.allSubjects
+        elif self.option.upper() == "department".upper(): infoList = self.allDepartments.keys()
+        elif self.option.upper() == "time/days".upper(): infoList = []
+        elif self.option.upper() == "professor".upper(): infoList = []
+                
+        length = len(infoList)
+        self.vars = {}
+        for i in range(length):
+            var = IntVar()
+            button = Checkbutton(self.frame, text=infoList[i], variable=var, justify=LEFT)
+            self.vars[infoList[i]] = var
+            button.pack(fill=X, anchor=W)
+        self.toSearchVars[self.option] = self.vars
+        
+
 
 app = CourseBrowserApp()
 app.mainloop()
