89fadf01dd49e8516631181098edd88413c7090e
mantaleigh <samanthalvoigt@gmail.com>
2015-05-24 19:48:20 -0700

    Improved modularization of HTML scraper

--- HTMLscraper.py
+++ HTMLscraper.py
@@ -3,10 +3,9 @@
 # last edit: 3.29.15
 
 from bs4 import BeautifulSoup as bs
+from bs4 import SoupStrainer
 import urllib2
 import ast
-
-#TODO -- Can this be more efficient than O(n^2)? (or, god... even worse)
 
 #note that the only thing this doesn't do is scrape departments and their acronyms
 #associated with it. Since, as far as I can see, there is no way to scrape every
@@ -25,7 +24,6 @@ class BrowserScraper():
     # returns a list of the distributions, scraped from the drop down menu at the top of the page
     def getDistributions(self):
         print "running getDistributions()"
-        #soup = getSoup()
         distributions = []
         dbox = self.soup.find(id='distribution')
         for option in dbox.find_all('option'):
@@ -34,7 +32,6 @@ class BrowserScraper():
 
     def getSubjects(self):
         print "running getSubjects()"
-        #soup = getSoup()
         subjects = []
         sbox = self.soup.find(id='subject')
         for option in sbox.find_all('option'):
@@ -45,118 +42,152 @@ class BrowserScraper():
         #eventually hopefully this will be able to do something of use...
         pass
 
+    # ------ methods that take information from a row of cells and saves it into a given dictionary ------
+    def saveCRN(self, rowCells, courseDict): 
+        if rowCells[0].contents != []:
+          courseDict['CRN'] = rowCells[0].string.encode('utf-8')
+
+    def saveTitle(self, rowCells, courseDict): 
+        if rowCells[2].contents != []:
+           courseDict['Title'] = rowCells[2].string.encode('utf-8')
+
+    def saveCurrentEnrollment(self, rowCells, courseDict): 
+        if rowCells[3].contents != []:
+            courseDict['Current Enrollment'] = rowCells[3].string.encode('utf-8')
+
+    def saveDistributions(self, rowCells, courseDict): 
+        if rowCells[10].contents != []: 
+            if len(rowCells[10].contents) == 1: # if there is only one distribution
+                courseDict['Distribution(s)'] = rowCells[10].string.encode('utf-8')
+            else: 
+                distList = []
+                for dist in rowCells[10].text.split('\n'): 
+                    distList.append(dist.encode('utf-8'))
+                courseDict['Distribution(s)'] = distList
+
+    def saveDaysAndTimes(self, rowCells, courseDict): 
+        timeCell = rowCells[6]
+        dayCell = rowCells[7]
+        if (timeCell.contents != [] and dayCell.contents != []):
+            dayTimeDict = {}
+            timeList = []
+            if len(timeCell.contents) > 1: # if there is more than one meeting time
+                for time in timeCell.text.split('\n'): 
+                    timeList.append(time.encode('utf-8'))
+            else: 
+                timeList.append(timeCell.string.encode('utf-8'))
+
+            # match up the days to the times
+            for i in range(1, len(dayCell.text.split('\n'))): # for each day section (usually only 1 or 2)
+                allDaysForOneTime = dayCell.text.split('\n')[i] # get all the days for a certain time
+                for day in ["Th", "M", "T", "W", "F"]: #Th must be first since it's two chars and might be confused with T
+                    if day in allDaysForOneTime: 
+                        dayTimeDict[day] = timeList[i-1] # save that day and corresponding time into the dict
+                        allDaysForOneTime = allDaysForOneTime.replace(day, "") # update the allDays var to be all days we haven't covered
+            courseDict['Day/Time'] = dayTimeDict
+
+    def saveCourse(self, rowCells, courseDict): 
+        if rowCells[1].find('a').contents != []:
+            courseDict['Course'] = rowCells[1].find('a').string.encode('utf-8')
+
+    def saveLocation(self, rowCells, courseDict): 
+        if rowCells[5].find('a').contents != []:
+            courseDict['Location(s)'] = rowCells[5].find('a').string.encode('utf-8')
+   
+    def saveInstructor(self, rowCells, courseDict): 
+        if rowCells[8].find('a').contents != []:
+            courseDict['Instructor'] = rowCells[8].find('a').string.encode('utf-8')
+
+    def saveAddInstructor(self, rowCells, courseDict): 
+        if rowCells[9].contents != []:
+            courseDict['Additional Instructor(s)'] = rowCells[9].find('a').string.encode('utf-8')
+
+    def saveSeatsAvail(self, rowCells, courseDict): 
+        cell = rowCells[4]
+        if cell.contents != []:
+            courseDict['Seats Available'] = cell.next.next.next.string.strip('\n').encode('utf-8')
+
+    def saveDescriptionAndPrereqs(self, rowCells, courseDict): 
+
+        # TODO - Making a new soup for the new page takes a long time. How can I quicken this? 
+
+        cell = rowCells[11]
+        # information lies in the "more" link
+        linkTag = str(cell.find('a'))
+        linkPt2 = linkTag[linkTag.find('=')+2:linkTag.find(' onclick')-1]
+        moreLink = ('http://courses.wellesley.edu/' + linkPt2).replace('&amp;','&')
+        page2 = urllib2.urlopen(moreLink)
+        #descriptionHTML = SoupStrainer(text="Description")
+        soup2 = bs(page2) #, parseOnlyThese=descriptionHTML)
+        print soup2
+
+        # # get description
+        descStr = ''
+        try: 
+            for string in soup2.find(text="Description").next.next.stripped_strings:
+                descStr = descStr + ' ' + string.encode('utf-8')
+        except AttributeError: # Except when the description is curiously blank...
+            descStr = 'Not provided'
+        courseDict['Description'] = descStr
+
+        # # get prereqs
+        try: 
+            prereqStr = soup2.find(text="Prerequisite(s)").next.next.string.encode('utf-8')
+        except AttributeError: # when there are no prereqs
+            prereqStr = 'Not provided'
+        courseDict['Prerequisite(s)'] = prereqStr
+
+
     def getAllCourses(self):
         '''Scrapes all course information from the Wellesley College Course Browser and
         returns a list of dictionaries corresponding to each course'''
-        listOfCourses = [] # will hold all the class dictionaries
-        cellNames = ['CRN', 'Course', 'Title', 'Current Enrollment', 'Seats Available', \
-        'Location(s)', 'Meeting Time(s)', 'Days(s)', 'Instructor', 'Additional Instructor(s)', \
-        'Distribution(s)', 'Description', 'Prerequisites']
-        #soup = getSoup()
+        dictOfCourses = {} # will hold all the class dictionaries
         tableContent = self.soup.find('tbody')
 
-    #    row = tableContent.find_all('tr')[424]
         for row in tableContent.find_all('tr'): # each row corresponds to an individual course
             indivClassDict = {}
-            timeList = []
             cellNum = 0
-            for cell in row.find_all('th'): #every loop is a new if statement that corresponds to cellNum
-                if cell.contents != []: # only want to save information if there is information to save...
+            cells = row.find_all('th')
 
-                    # ---------- CRN, TITLE, CURRENT ENROLLMENT, DISTRIBUTION (IF ONLY ONE) -----------
-                    if cellNum == 0 or cellNum == 2 or cellNum == 3 or (cellNum == 10 and len(cell.contents) == 1):
-                        indivClassDict[cellNames[cellNum]] = cell.string.encode('utf-8')
+            self.saveCRN(cells, indivClassDict)
+            self.saveCourse(cells, indivClassDict)
+            self.saveTitle(cells, indivClassDict)
+            self.saveCurrentEnrollment(cells, indivClassDict)
+            self.saveSeatsAvail(cells, indivClassDict)
+            self.saveLocation(cells, indivClassDict)
+            self.saveDaysAndTimes(cells, indivClassDict)
+            self.saveInstructor(cells, indivClassDict)
+            self.saveAddInstructor(cells, indivClassDict)
+            self.saveDistributions(cells, indivClassDict)
+            self.saveDescriptionAndPrereqs(cells, indivClassDict)
 
-                    # ----------- MULTIPLE DISTRIBUTIONS ----------------------------------------------
-                    elif (cellNum == 10 and len(cell.contents) > 1): #when there is more than 1 distribution
-                        distList = []
-                        for dist in cell.text.split('\n'):
-                            distList.append(dist.encode('utf-8'))
-                        indivClassDict[cellNames[cellNum]] = distList # save the list of distributions into the course dict
-
-                    # ----------- MEETING TIME(S) -----------------------------------------------------
-                    elif cellNum == 6:
-                        if len(cell.contents) > 1: # if there is more than one meeting time
-                            for time in cell.text.split('\n'):
-                                timeList.append(time.encode('utf-8')) # append each time as a different elt in the list
-                        else:
-                            timeList.append(cell.string.encode('utf-8')) # otherwise append the single time to the list
-
-                    # ----------- MATCHES DAYS WITH TIME(S) -------------------------------------------
-                    elif cellNum == 7:
-                        dayTimeDict = {} # dictionary to hold corresponding days and times
-                        for i in range(1, len(cell.text.split('\n'))): # for each day section (usually only 1 or 2)
-                            allDaysForOneTime = cell.text.split('\n')[i]# get all the days for a certain time
-                            for day in ["Th", "M", "T", "W", "F"]: #Th must be first since it's two chars and might be confused with T
-                                if day in allDaysForOneTime:
-                                    dayTimeDict[day] = timeList[i-1] #save that day and corresponding time into the dict
-                                    allDaysForOneTime = allDaysForOneTime.replace(day,"") # update the allDays var to be all days we haven't covered
-                        indivClassDict['Day/Time'] = dayTimeDict
-
-                    elif cellNum == 1 or cellNum == 5 or cellNum == 8 or cellNum == 9:
-                        # save Course, Location(s), and Instructor and Additional Instructor
-                        if cell.find('a').contents != []:
-                            indivClassDict[cellNames[cellNum]] = cell.find('a').string.encode('utf-8')
-
-                    elif cellNum == 4:
-                        # save Seats Available
-                        indivClassDict[cellNames[cellNum]] = cell.next.next.next.string.strip('\n').encode('utf-8')
-
-                    # save description and the prereqs from the 'more' link
-                    elif cellNum == 11:
-                        linkTag = str(cell.find('a'))
-                        linkPt2 = linkTag[linkTag.find('=')+2:linkTag.find(' onclick')-1]
-                        moreLink = ('http://courses.wellesley.edu/' + linkPt2).replace('&amp;','&')
-                        page2 = urllib2.urlopen(moreLink)
-                        soup2 = bs(page2)
-
-                        # get description:
-                        descStr = ''
-                        try:
-                            for string in soup2.find(text="Description").next.next.stripped_strings:
-                                descStr = descStr + ' ' + string.encode('utf-8')
-                        except AttributeError: # Except when the description is curiously blank...
-                            descStr = 'Not provided'
-                        indivClassDict[cellNames[cellNum]] = descStr
-
-                        # get prereqs:
-                        try:
-                            prereqStr = soup2.find(text="Prerequisite(s)").next.next.string.encode('utf-8')
-                        except AttributeError: # when there is no prereq
-                            prereqStr = 'Not provided'
-                        indivClassDict['Prerequisite(s)'] = prereqStr
-
-                #needs to be a separate if statement, not an elif
-                if cellNum <= len(cellNames):
-                    cellNum += 1
-
-            listOfCourses.append(indivClassDict) #should go inside the first loop, but outside the second
+            dictOfCourses[indivClassDict['CRN']] = indivClassDict #should go inside the first loop, but outside the second
             print "Saving course with CRN: " + indivClassDict['CRN'] #since it takes a fair amount of time to scrape all the data, this helps know that the program is running
-        return listOfCourses
+        return dictOfCourses
 
     # ------------------------------------------------------------------------------
     #                                WRITE INFO
     # ------------------------------------------------------------------------------
 
-    def writeCoursesFile(self, L):
-        '''Given a list of courses, writes a file with the list'''
+    def writeCoursesFile(self, D):
+        '''Given a dict of courses, writes a file with the dict'''
         filename = 'coursesFile.txt'
         courseFile = open(filename,'w')
-        courseFile.write(str(L))
+        courseFile.write(str(D))
         courseFile.close()
 
-    def writeDistFile(self, L):
-        '''Given a list of distributions, writes a file with the list'''
+    def writeDistFile(self, D):
+        '''Given a dict of distributions, writes a file with the dict'''
         filename = 'distributionFile.txt'
         distFile = open(filename, 'w')
-        distFile.write(str(L))
+        distFile.write(str(D))
         distFile.close()
 
-    def writeSubjFile(self, L):
-        '''Given a list of subjects, writes a file with the list'''
+    def writeSubjFile(self, D):
+        '''Given a dict of subjects, writes a file with the dict'''
         filename = 'subjectFile.txt'
         subjFile = open(filename, 'w')
-        subjFile.write(str(L))
+        subjFile.write(str(D))
         subjFile.close()
 
     # agh anger how make dept scraping work
@@ -218,7 +249,7 @@ class BrowserScraper():
         self.writeCoursesFile(listOfCourses)
         return listOfCourses
 
-    def updateAllExceptCourses(se,f):
+    def updateAllExceptCourses(self):
         '''scrapes and writes/rewrites all info except courses. Returns None'''
         self.writeDistFile(self.getDistributions())
         self.writeSubjFile(self.getSubjects())
